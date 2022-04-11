6f56e888fdd6143001b371530ea968d14e7572b8
Aman Sharma <shrma.aman7@gmail.com>
2022-04-11 17:48:23 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by AMAN SHARMA on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
